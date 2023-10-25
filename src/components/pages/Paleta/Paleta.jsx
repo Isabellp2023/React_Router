3301f28ac3cf1ac56682676fef17cd90009e175e
MARIA ISABEL LOPEZ GARCIA <isabelopez.cursos@gmail.com>
2023-10-25 22:52:16 +0200

    retocado2

--- src/components/pages/Paleta/Paleta.jsx
+++ src/components/pages/Paleta/Paleta.jsx
@@ -1,11 +1,17 @@
 import "./Paleta.css";
+
 const Paleta = () => {
-    return(
-        <>
+    return (
         <div className="portatil">
-        <img src="public/ordenador.jpg" alt="Un ordenador" />
+            <div className="image-container">
+                <img src="/public/gaming.png" alt="Un ordenador" />
+                <div className="texto">  
+                <h1>Full Stack Developer</h1>
+                <p>Un FullStack developer es un profesional versátil que domina tanto el desarrollo front-end como el back-end de aplicaciones web, lo que les permite trabajar en proyectos completos de extremo a extremo.</p>
+                </div>
+            </div>
         </div>
-        </>
-    )
+    );
 }
+
 export default Paleta;
